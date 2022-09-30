ac1c9d12068a6f299c8b87567c366e0b835d39f5
Vandenvin <sevandenvin@gmail.com>
2022-09-30 15:00:17 +0000

    Deploying to gh-pages from @ Vandenvin/portfolio@f41479ca4eaa0548b276e024b00b7612c72fe156 🚀

---
+++ partials/05-fontsize.2fe7d21f.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("#font-size-control"),t=document.querySelector("#text");e.addEventListener("input",(function(e){var n=e.currentTarget.value;console.log(n),t.style.fontSize="".concat(n,"px")}))}();
+//# sourceMappingURL=05-fontsize.2fe7d21f.js.map
---
+++ partials/05-fontsize.e0fb2c32.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#font-size-control"),t=document.querySelector("#text");e.addEventListener("input",(function(e){const n=e.currentTarget.value;console.log(n),t.style.fontSize=`${n}px`}));
+//# sourceMappingURL=05-fontsize.e0fb2c32.js.map
