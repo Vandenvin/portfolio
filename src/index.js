134ffde2b2c1f40191abd21b81e0f44a32872e0a
Serhii Vandenvin <sevandenvin@gmail.com>
2022-09-30 19:36:10 +0300

    added button CV

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-const headerIconTel = document.querySelector('.js-header-icon-tel');
-const headerIconMail = document.querySelector('.js-header-icon-mail');
-const headerMail = document.querySelector('.js-header-mail');
-const headerTel = document.querySelector('.js-header-tel');
+// const headerIconTel = document.querySelector('.js-header-icon-tel');
+// const headerIconMail = document.querySelector('.js-header-icon-mail');
+// const headerMail = document.querySelector('.js-header-mail');
+// const headerTel = document.querySelector('.js-header-tel');
 
-headerIconTel.addEventListener('click', onHeaderIconTelClick);
-headerIconMail.addEventListener('click', onHeaderIconMailClick);
+// headerIconTel.addEventListener('click', onHeaderIconTelClick);
+// headerIconMail.addEventListener('click', onHeaderIconMailClick);
 
-function onHeaderIconTelClick() {
-  headerMail.classList.add('hidden');
-  headerTel.classList.toggle('hidden');
-}
-function onHeaderIconMailClick() {
-  headerTel.classList.add('hidden');
-  headerMail.classList.toggle('hidden');
-}
+// function onHeaderIconTelClick() {
+//   headerMail.classList.add('hidden');
+//   headerTel.classList.toggle('hidden');
+// }
+// function onHeaderIconMailClick() {
+//   headerTel.classList.add('hidden');
+//   headerMail.classList.toggle('hidden');
+// }
