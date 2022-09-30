af23b2933b2eda0355089bd7e2c033296b65317c
Vandenvin <sevandenvin@gmail.com>
2022-09-30 16:02:36 +0000

    Deploying to gh-pages from @ Vandenvin/portfolio@b46c743453f1dd3db4de8bfad9ed68c8240e1688 🚀

---
+++ partials/04-counter.c264df87.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-action=decrement]"),t=document.querySelector("[data-action=increment]"),n=document.querySelector("#value"),c=0;e.addEventListener("click",(function(){return c-=1,n.textContent=c})),t.addEventListener("click",(function(){return c+=1,n.textContent=c}))}();
+//# sourceMappingURL=04-counter.c264df87.js.map
---
+++ partials/04-counter.f4978e09.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-action=decrement]"),t=document.querySelector("[data-action=increment]"),n=document.querySelector("#value");let c=0;e.addEventListener("click",(()=>(c-=1,n.textContent=c))),t.addEventListener("click",(()=>(c+=1,n.textContent=c)));
+//# sourceMappingURL=04-counter.f4978e09.js.map
