7008b9e05e669221b00d1ee20ea795ed7cf93913
Vandenvin <sevandenvin@gmail.com>
2022-10-07 12:18:34 +0000

    Deploying to gh-pages from @ Vandenvin/portfolio@05b3096ec84e6b97b1a86996e4b7f7bbe9a417ef 🚀

---
+++ index.d436e251.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".js-open-menu"),s=document.querySelector(".js-close-menu"),n=document.querySelector(".js-menu-container");e.addEventListener("click",(function(){e.classList.remove("is-open"),n.classList.add("is-open"),s.classList.add("is-open")})),s.addEventListener("click",(function(){s.classList.remove("is-open"),n.classList.remove("is-open"),e.classList.add("is-open")}))}();
+//# sourceMappingURL=index.d436e251.js.map
